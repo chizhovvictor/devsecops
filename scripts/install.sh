41a9d8b37c79b40af4e2728960b4b617cf7b5583
Victor Chizhov <victor.chizhov93@gmail.com>
2025-11-26 14:24:25 +0100

    Практически ебанул

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,26 +14,61 @@ helm repo update
 echo "Install ingress-nginx"
 helm install ingress-nginx ingress-nginx/ingress-nginx
 
+# 3.  развертывание приложения finenomore
+echo "Create namespace for finenomore"
+kubectl create namespace finenomore
 
+echo "install finenomore app"
+helm install finenomore my_project/k8s/finenomore 
 
+# 4. установка gitlab-runner через helm в кластер
 echo "Add helm repo for gitlab-runner"
 helm repo add gitlab https://charts.gitlab.io
 
 echo "Install gitlab-runner"
-helm install --namespace default gitlab-runner -f ./runners/values.yaml gitlab/gitlab-runner
+helm install --namespace default gitlab-runner -f scripts/runners/values.yaml gitlab/gitlab-runner
 
-echo "Create namespace for finenomore"
-kubectl create namespace finenomore
-
+# 5. создание секрета для доступа к gitlab container registry (это пункт 3)
 echo "Create secret for gitlab credentials"
-kubectl create secret docker-registry gitlab-credentials --docker-server=registry.gitlab.com --docker-username=chizhovvictor --docker-password=<PASSWORD> --docker-email=test@test.com -n finenomore
+kubectl create secret docker-registry gitlab-credentials --docker-server=registry.gitlab.com --docker-username=chizhovvictor --docker-password=<ACCESS TOKEN> --docker-email=test@test.com -n finenomore
 
+# 6. установка gitlab-agent в кластер для автоматического подключения к gitlab
 echo "Create gitlab agent in kubernetes"
 helm repo add gitlab https://charts.gitlab.io
 helm repo update
-helm upgrade --install test gitlab/gitlab-agent \
+helm upgrade --install finenomore-gitlab-agent gitlab/gitlab-agent \
     --namespace gitlab-agent-test \
     --create-namespace \
     --set config.token=<TOKEN> \
     --set config.kasAddress=wss://kas.gitlab.com
 
+
+# 7. установка DAST в кластер
+echo "Create namespace for DAST"
+kubectl create namespace finenomore-dast
+
+# 8. установка секрета для доступа к gitlab container registry для DAST пространства
+echo "Install secret registry for DAST"
+kubectl create secret docker-registry gitlab-credentials --docker-server=registry.gitlab.com --docker-username=chizhovvictor --docker-password=<ACCESS TOKEN> --docker-email=test@test.com -n finenomore-dast
+
+# 9. установка defectdojo в кластер для дашборда уязвимостей
+echo "Create namespace for defectdojo"
+kubectl create namespace defectdojo
+
+echo "Add helm repo for defectdojo"
+helm repo add defectdojo https://raw.githubusercontent.com/DefectDojo/django-DefectDojo/helm-charts
+helm repo add yc-courses-ru-devsecops-helm-charts https://yandex-cloud-examples.github.io/yc-courses-ru-devsecops-helm-charts/
+
+echo "Install defectdojo"
+helm install --namespace defectdojo defectdojo defectdojo/defectdojo --version --values defectdojo/values-custom.yaml --set createRabbitMqSecret=true
+helm install defectdojo yc-courses-ru-devsecops-helm-charts/defectdojo --namespace defectdojo --create-namespace --values ./defectdojo/values-custom.yaml --set createRabbitMqSecret=true
+# 10. вывод пароля для доступа к defectdojo
+echo "DefectDojo admin password: $(kubectl \
+      get secret defectdojo \
+      --namespace=defectdojo \
+      --output jsonpath='{.data.DD_ADMIN_PASSWORD}' \
+      | base64 --decode)"
+
+
+
+defectdojo.159.89.241.129.sslip.io
--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "digitalocean_project" "devsecops" {
 resource "digitalocean_kubernetes_cluster" "k8s" {
   name    = "devsecops"
   region  = "nyc3"
-  version = "1.32.5-do.3"
+  version = "1.34.1-do.0"
 
   # vpc_uuid = digitalocean_vpc.devsecops_vpc.id
 
@@ -31,7 +31,7 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
 
   node_pool {
     name       = "default-pool"
-    size       = "s-4vcpu-8gb"
+    size       = "s-4vcpu-16gb-amd"
     node_count = 1
 
     tags       = ["k8s-node"]
@@ -43,12 +43,12 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
 }
 
 # 4. Привязка кластера к проекту (обязательно зависит от кластера и проекта)
-# resource "digitalocean_project_resources" "attach_cluster" {
-#   project = digitalocean_project.devsecops.id
-#   resources = [
-#     digitalocean_kubernetes_cluster.k8s.urn,
-#   ]
-# }
+resource "digitalocean_project_resources" "attach_cluster" {
+  project = digitalocean_project.devsecops.id
+  resources = [
+    digitalocean_kubernetes_cluster.k8s.urn,
+  ]
+}
 
 # 5. Container Registry (опционально можно тоже привязать к проекту, но пока без этого)
 # resource "digitalocean_container_registry" "default" {
